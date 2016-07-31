96a90b6efd2a4e6c77dedd311071fd17e1c29372
Morasthite <Morasthite@gmail.com>
2016-07-31 21:26:17 +0000

    begin

--- index.php
+++ index.php
@@ -26,12 +26,12 @@ $fb = new Facebook\Facebook([
   <title>Food Decider</title>
   <!--Bootstrap CDN -->
   <script src ="https://code.jquery.com/jquery-2.1.4.min.js"></script>
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
   <!--jQuery CDN-->
   <!--Angular CDN with Angular routing CDN-->
   <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script>
-  <script src="http://code.angularjs.org/1.3.15/angular-route.js"></script>
+  <script src="https://code.angularjs.org/1.3.15/angular-route.js"></script>
   <!--Google Fonts-->
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Pacifico">
   <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans">
